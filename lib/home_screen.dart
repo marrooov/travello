e693edc38eed57b869ca5ff935d36c5e10b34c8b
marrooov <merogamer999@gmail.com>
2026-07-05 02:41:50 +0300

    finishing home screen with navigation bar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,9 +1,57 @@
 import 'package:flutter/material.dart';
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+int currentIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text('Home'),
+        ),
+      ),
+    bottomNavigationBar: BottomNavigationBar(
+  currentIndex: currentIndex,
+
+  onTap: (index) {
+    setState(() {
+      currentIndex = index;
+    });
+  },
+  type: BottomNavigationBarType.fixed,
+  backgroundColor: const Color(0xFFFF4B67),
+  selectedItemColor: Colors.black,
+  unselectedItemColor: Colors.white,
+  selectedFontSize: 14,
+  unselectedFontSize: 14,
+    items:  [
+     BottomNavigationBarItem(
+        icon: Icon(Icons.home_outlined),
+        label: 'Home',
+      ),
+
+     BottomNavigationBarItem(
+        icon: Icon(Icons.shopping_cart_outlined),
+        label: 'Cart',
+        ),
+
+      BottomNavigationBarItem(
+        icon: Icon(Icons.favorite_border),
+        label: 'Favourite',
+       ),
+
+       BottomNavigationBarItem(
+         icon: Icon(Icons.person_outline),
+         label: 'Account',
+        ),
+        ],
+      ),
+    );
   }
 }
